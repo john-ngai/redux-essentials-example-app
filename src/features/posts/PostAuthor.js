9adb8b3cbf3e284e5bfdf610f1a03fc6f30b73e4
mrjohnming <john.ngai@hotmail.com>
2022-07-30 17:36:55 -0400

    Replace 'id' with 'userId' inside the .find() method

--- src/features/posts/PostAuthor.js
+++ src/features/posts/PostAuthor.js
@@ -2,7 +2,7 @@ import { useSelector } from 'react-redux'
 
 export const PostAuthor = ({ userId }) => {
   const author = useSelector((state) =>
-    state.users.find((user) => user.id === id)
+    state.users.find((user) => user.id === userId)
   )
 
   return <span>by {author ? author.name : 'unknown author'}</span>
