5bce53c870e6e1744b84c4cbcba93826a22bd0c2
mrjohnming <john.ngai@hotmail.com>
2022-07-30 16:58:56 -0400

    Add reducer - users

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
+// Redux reducers
 import postsReducer from '../features/posts/postsSlice'
+import usersReducer from '../features/users/usersSlice'
 
 export default configureStore({
   reducer: {
     posts: postsReducer,
+    users: usersReducer,
   },
 })
