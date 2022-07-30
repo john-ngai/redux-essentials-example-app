93daff2256c3f8496ed8a2c05eeda0b487e4ca5d
mrjohnming <john.ngai@hotmail.com>
2022-07-30 17:57:43 -0400

    Add onClick handler

--- src/features/posts/ReactionButtons.js
+++ src/features/posts/ReactionButtons.js
@@ -1,15 +1,27 @@
-const reactionEmoji = {
-  thumbsUp: '👍',
-  hooray: '🎉',
-  heart: '❤️',
-  rocket: '🚀',
-  eyes: '👀',
-}
+import { useDispatch } from 'react-redux'
+import { reactionAdded } from './postsSlice'
 
 export const ReactionButtons = ({ post }) => {
+  const reactionEmoji = {
+    thumbsUp: '👍',
+    hooray: '🎉',
+    heart: '❤️',
+    rocket: '🚀',
+    eyes: '👀',
+  }
+
+  const dispatch = useDispatch()
+
   const reactionButtons = Object.entries(reactionEmoji).map(([name, emoji]) => {
     return (
-      <button key={name} type="button" className="muted-button reaction-button">
+      <button
+        className="muted-button reaction-button"
+        type="button"
+        key={name}
+        onClick={() =>
+          dispatch(reactionAdded({ postId: post.id, reaction: name }))
+        }
+      >
         {emoji} {post.reactions[name]}
       </button>
     )
