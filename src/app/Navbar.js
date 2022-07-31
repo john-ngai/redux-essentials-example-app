2d59ad96bad32955e9bb1e6213c26fc77c09e462
mrjohnming <john.ngai@hotmail.com>
2022-07-31 10:40:23 -0400

    Add <Link /> for notifications & button to fetch new data

--- src/app/Navbar.js
+++ src/app/Navbar.js
@@ -1,17 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { fetchNotifications } from '../features/notifcations/notificationsSlice'
 
 export const Navbar = () => {
+  const dispatch = useDispatch()
+
+  const fetchNewNotifications = () => {
+    dispatch(fetchNotifications())
+  }
+
   return (
     <nav>
       <section>
         <h1>Redux Essentials Example</h1>
-
         <div className="navContent">
           <div className="navLinks">
             <Link to="/">Posts</Link>
             <Link to="/users">Users</Link>
+            <Link to="/notifcations">Notifications</Link>
           </div>
+          <button className="button" onClick={fetchNewNotifications}>
+            Refresh Notifications
+          </button>
         </div>
       </section>
     </nav>
