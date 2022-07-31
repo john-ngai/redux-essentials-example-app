245394c16a7c4926b5e9df9c785af87c15ef4911
mrjohnming <john.ngai@hotmail.com>
2022-07-31 10:18:22 -0400

    Add <Routes /> for /users

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import {
   BrowserRouter as Router,
-  Switch, Route, Redirect,
+  Switch,
+  Route,
+  Redirect,
 } from 'react-router-dom'
 // Components
 import { Navbar } from './app/Navbar'
@@ -8,6 +10,8 @@ import { PostsList } from './features/posts/PostsList'
 import { AddPostForm } from './features/posts/AddPostForm'
 import { SinglePostPage } from './features/posts/SinglePostPage'
 import { EditPostForm } from './features/posts/EditPostForm'
+import { UsersList } from './features/users/UsersList'
+import { UserPage } from './features/users/UserPage'
 
 export default function App() {
   return (
@@ -27,6 +31,8 @@ export default function App() {
           />
           <Route exact path="/posts/:postId" component={SinglePostPage} />
           <Route exact path="/editPost/:postId" component={EditPostForm} />
+          <Route exact path="/users" component={UsersList} />
+          <Route exact path="/users/:userId" component={UserPage} />
           <Redirect to="/" />
         </Switch>
       </div>
