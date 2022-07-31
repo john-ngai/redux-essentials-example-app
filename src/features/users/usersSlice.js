8efb1cbe66c89d481ac85d02e61c5effcafdcfdc
mrjohnming <john.ngai@hotmail.com>
2022-07-31 07:54:20 -0400

    Create thunk - fetchUsers

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,11 +1,22 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk, bindActionCreators } from '@reduxjs/toolkit'
+import { client } from '../../api/client'
 
 const initialState = []
+
+const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+  const response = await client.get('/fakeApi/users')
+  return response.data
+})
 
 const usersSlice = createSlice({
   name: 'users',
   initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      return action.payload
+    })
+  }
 })
 
 export default usersSlice.reducer
