9191eb9484789b8fdcf60635c60395fd89a3a7b2
mrjohnming <john.ngai@hotmail.com>
2022-07-31 08:01:42 -0400

    Fetch users from API on start

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
+// Packages
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+// Root component
 import App from './App'
-import store from './app/store'
+// Redux
 import { Provider } from 'react-redux'
-
+import store from './app/store'
+import { fetchUsers } from './features/users/usersSlice'
+// Mock API server
 import { worker } from './api/server'
+// Stylesheet
+import './index.css'
 
-// Wrap app rendering so we can wait for the mock API to initialize
+// Wrap app rendering so we can wait for the mock API to initialize.
 async function start() {
-  // Start our mock API server
+  // Start our mock API server.
   await worker.start({ onUnhandledRequest: 'bypass' })
+
+  // Users only need to be fetched once, so this should be performed on start.
+  store.dispatch(fetchUsers())
 
   ReactDOM.render(
     <React.StrictMode>
