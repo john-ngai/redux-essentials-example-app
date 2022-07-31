3f20488a8220cb2fdab5abb676e23e3c909b4b71
mrjohnming <john.ngai@hotmail.com>
2022-07-31 10:43:07 -0400

    Remove unnecessary comments

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit'
-// Redux reducers
 import postsReducer from '../features/posts/postsSlice'
 import usersReducer from '../features/users/usersSlice'
 import notificationsReducer from '../features/notifications/notificationsSlice'
