3d18e5ed5ed694f55e02225eec04b2f1b2c21f31
mrjohnming <john.ngai@hotmail.com>
2022-08-01 11:20:28 -0400

    Convert state to a normalized data structure via createEntityAdapter

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,7 +1,13 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import { client } from '../../api/client'
+import {
+  createEntityAdapter,
+  createSlice,
+  createAsyncThunk,
+} from '@reduxjs/toolkit'
 
-const initialState = []
+const usersAdapter = createEntityAdapter()
+
+const initialState = usersAdapter.getInitialState()
 
 export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
   const response = await client.get('/fakeApi/users')
@@ -13,15 +19,13 @@ const usersSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers(builder) {
-    builder.addCase(fetchUsers.fulfilled, (state, action) => {
-      return action.payload
-    })
+    // usersAdapter.setAll can be used when we want to replace the entire
+    // existing users array with the one fetched from the server.
+    builder.addCase(fetchUsers.fulfilled, usersAdapter.setAll)
   },
 })
 
 export default usersSlice.reducer
 
-export const selectAllUsers = (state) => state.users
-
-export const selectUserById = (state, userId) =>
-  state.users.find((user) => user.id === userId)
+export const { selectAll: selectAllUsers, selectById: selectUserById } =
+  usersAdapter.getSelectors((state) => state.users)
