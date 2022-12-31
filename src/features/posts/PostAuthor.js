5d16a50cfbaeb15763740346863569c71b9ff7fa
John Ngai <jongai@blackberry.com>
2022-12-31 11:21:17 -0500

    Reset all existing files into the initial example project setup

--- src/features/posts/PostAuthor.js
+++
@@ -1,8 +0,0 @@
-import { useSelector } from 'react-redux'
-import { selectUserById } from '../users/usersSlice'
-
-export const PostAuthor = ({ userId }) => {
-  const author = useSelector((state) => selectUserById(state, userId))
-
-  return <span>by {author ? author.name : 'unknown author'}</span>
-}
--- src/index.js
+++ src/index.js
@@ -1,24 +1,16 @@
-// Packages
 import React from 'react'
 import ReactDOM from 'react-dom'
-// Root component
-import App from './App'
-// Redux
-import { Provider } from 'react-redux'
-import store from './app/store'
-import { fetchUsers } from './features/users/usersSlice'
-// Mock API server
-import { worker } from './api/server'
-// Stylesheet
 import './index.css'
+import App from './App'
+import store from './app/store'
+import { Provider } from 'react-redux'
 
-// Wrap app rendering so we can wait for the mock API to initialize.
+import { worker } from './api/server'
+
+// Wrap app rendering so we can wait for the mock API to initialize
 async function start() {
-  // Start our mock API server.
+  // Start our mock API server
   await worker.start({ onUnhandledRequest: 'bypass' })
-
-  // Users only need to be fetched once, so this should be performed on start.
-  store.dispatch(fetchUsers())
 
   ReactDOM.render(
     <React.StrictMode>
