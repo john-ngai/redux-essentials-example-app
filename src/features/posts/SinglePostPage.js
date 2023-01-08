a0ef6629c4b5e5853fbbb04c11562ff282abbdf5
John Ngai <jongai@blackberry.com>
2023-01-08 10:37:23 -0500

    Changes from Part 7: RTK Query Basics

--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -1,27 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-
+import { Spinner } from '../../components/Spinner'
+import { useGetPostQuery } from '../api/apiSlice'
 import { PostAuthor } from './PostAuthor'
-import { TimeAgo } from './TimeAgo'
 import { ReactionButtons } from './ReactionButtons'
-import { selectPostById } from './postsSlice'
+import { TimeAgo } from './TimeAgo'
 
 export const SinglePostPage = ({ match }) => {
   const { postId } = match.params
 
-  const post = useSelector((state) => selectPostById(state, postId))
+  const { data: post, isFetching, isSuccess, isError } = useGetPostQuery(postId)
 
-  if (!post) {
-    return (
-      <section>
-        <h2>Post not found!</h2>
-      </section>
-    )
-  }
-
-  return (
-    <section>
+  let content
+  if (isFetching) {
+    content = <Spinner text="Loading..." />
+  } else if (isSuccess) {
+    content = (
       <article className="post">
         <h2>{post.title}</h2>
         <div>
@@ -34,6 +28,10 @@ export const SinglePostPage = ({ match }) => {
           Edit Post
         </Link>
       </article>
-    </section>
-  )
+    )
+  } else if (isError) {
+    content = <h2>Post not found!</h2>
+  }
+
+  return <section>{content}</section>
 }
